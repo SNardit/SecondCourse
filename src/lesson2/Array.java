f9c610321165a12cca1f3ac5ca55e32b35e62464
Svetlana Nardit <s.nardit@gmail.com>
2020-03-30 22:42:21 -0400

    lesson1

--- src/lesson2/Array.java
+++ src/lesson2/Array.java
@@ -1,6 +1,7 @@
 package lesson2;
 
-import javax.lang.model.type.NullType;
+
+
 
 public class Array {
     private static final int SIZE = 4;
@@ -9,7 +10,10 @@ public class Array {
 
         try {
             String [][] arr = new String[SIZE][SIZE];
-            System.out.println("Сумма чисел в массиве равна " + array(arr));
+            array(arr);
+            arr [1][2] = "Oh";
+            arr [3][2] = null;
+            System.out.println("Сумма чисел в массиве равна " + sumArray(arr));
         }
         catch (MyArraySizeException | MyArrayDataException | MyNullException e) {
             System.out.println(e.getMessage());
@@ -17,24 +21,24 @@ public class Array {
 
     }
 
-    public static int array (String [][] arr ) {
-        if (arr.length != 4) {
+    public static void array (String [][] arr ) {
+
+        if (arr.length != 4 || arr[0].length != 4) {
             throw new MyArraySizeException();
         }
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                arr[i][j] = "0" + i+j;
+                arr[i][j] = "0" + i + j;
             }
         }
-        arr [1][2] = "Oh";
-        arr [3][2] = null;
+    }
 
+    public static int sumArray (String [][] arr ) {
         int sum = 0;
-
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
 
-                if ((String) arr[i][j] == null) {
+                if (arr[i][j] == null) {
                     throw new MyNullException(i, j);
                 }
                 if (arr[i][j].matches("\\D*")) {
@@ -45,7 +49,6 @@ public class Array {
 
             }
         }
-
         return sum;
     }
 
